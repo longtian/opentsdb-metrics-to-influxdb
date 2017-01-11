f98cdcec13a752477ebc0ed8688d490a2337178d
yan <wyvernnot@gmail.com>
2017-01-11 12:37:28 +0800

    refactoring, readability and logging

--- convert.py
+++ convert.py
@@ -3,17 +3,41 @@ import time
 import os
 import logging
 import sys
+import raven
+
+# 生成 logger 对象
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# 集成 raven
+client = None
+raven_sdn = os.getenv('RAVEN_SDN', None)
+if raven_sdn:
+    client = raven.Client(
+        dsn=raven_sdn
+    )
 
 
 def querify(params):
-    if len(params) == 0:
+    """
+    把 {a:1,b:2} 转换成 ,a=1,b=1 的格式
+    :param params: dict
+    :return: str
+    """
+    items = params.items()
+    if len(items) == 0:
         return ''
-    tags = [k + '=' + v for (k, v) in params.items()]
+    tags = [str(k) + '=' + str(v) for (k, v) in items]
+    return ',' + ','.join(sorted(tags))
 
-    return ',' + ','.join(tags)
 
-
-def convert(obj):
+def line(obj):
+    """
+    把 dict 转成 string， 参考 http://opentsdb.net/docs/build/html/api_http/stats/index.html
+    :param obj: dict
+    :return: str
+    """
     return '{metric}{tags} value={value} {timestamp}000000000'.format(
         metric=obj['metric'],
         timestamp=obj['timestamp'],
@@ -22,28 +46,59 @@ def convert(obj):
     )
 
 
-def main():
+def convert(obj):
+    """
+    :param obj:
+    :return:
+    """
+    return '\n'.join([line(item) for item in obj])
+
+
+def pipe(opentsdb_url, influxdb_url, timeout):
+    """
+    请求 Opentsdb => 转换格式 => 发送到 Influxdb
+    :return:
+    """
+
+    res = requests.get(opentsdb_url, timeout=timeout)
+
+    d = requests.post(influxdb_url, proxies={
+        "http": None
+    }, data=convert(res.json()))
+
+    status_code = d.status_code
+
+    # http://opentsdb.net/docs/build/html/api_http/index.html#response-codes
+    if status_code in [200, 204]:
+        logger.info('[%d] pipe success' % d.status_code)
+    else:
+        logger.error('[%d] pipe error:\n%s' % (d.status_code, d.text))
+
+
+if __name__ == '__main__':
+    # 从环境变量中读取配置
+
+    interval = int(os.getenv('INTERVAL', '10'))
     opentsdb_url = os.getenv('OPENTSDB_URL', None)
     influxdb_url = os.getenv('INFLUXDB_URL', None)
+
+    logger.info('interval=%d' % interval)
+    logger.info('opentsdb_url=%s' % opentsdb_url)
+    logger.info('influxdb_url=%s' % influxdb_url)
 
     if not opentsdb_url or not influxdb_url:
         sys.exit()
 
-    res = requests.get(opentsdb_url, timeout=1)
-
-    payload_lines = [convert(item) for item in res.json()]
-    d = requests.post(influxdb_url, proxies={
-        "http": None
-    }, data='\n'.join(payload_lines))
-
-    logging.info('[%d] write %d records' % (d.status_code, len(payload_lines)))
-
-
-if __name__ == '__main__':
     while True:
         try:
-            main()
+            pipe(
+                opentsdb_url=opentsdb_url,
+                influxdb_url=influxdb_url,
+                timeout=interval / 2
+            )
         except BaseException as e:
-            logging.error(e)
+            logger.error(e)
+            if client:
+                client.captureException()
         finally:
-            time.sleep(20)
+            time.sleep(interval)
--- tests/TestConvert.py
+++ tests/TestConvert.py
@@ -3,7 +3,7 @@ import convert
 
 
 class TestCovert(unittest.TestCase):
-    def test_convert(self):
+    def test_line(self):
         input = {
             "metric": "tsd.connectionmgr.exceptions",
             "timestamp": 1484051997,
@@ -14,6 +14,12 @@ class TestCovert(unittest.TestCase):
             }
         }
 
-        output = "tsd.connectionmgr.exceptions,type=timeout,host=xxx value=0 1484051997000000000"
+        output = "tsd.connectionmgr.exceptions,host=xxx,type=timeout value=0 1484051997000000000"
 
-        self.assertEqual(output, convert.convert(input))
+        self.assertEqual(output, convert.line(input))
+
+    def test_querify(self):
+        self.assertEqual('', convert.querify({}))
+        self.assertEqual(',a=1', convert.querify({"a": "1"}))
+        self.assertEqual(',a=1,b=2', convert.querify({"a": "1", "b": "2"}))
+        self.assertEqual(',a=1,b=2', convert.querify({"b": "2", "a": "1"}))
